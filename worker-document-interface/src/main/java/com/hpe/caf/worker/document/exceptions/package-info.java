28a990510cba057967fbb307201de1fffe0badad
Dermot Hardy <dermot.hardy@hpe.com>
2017-01-04 10:12:55 +0000

    Made a correction to a comment

--- worker-document-interface/src/main/java/com/hpe/caf/worker/document/exceptions/package-info.java
+++ worker-document-interface/src/main/java/com/hpe/caf/worker/document/exceptions/package-info.java
@@ -1,4 +1,4 @@
 /**
- * This package contains exceptions which may be used Document Worker implementations.
+ * This package contains exceptions which may be used by Document Worker implementations.
  */
 package com.hpe.caf.worker.document.exceptions;
