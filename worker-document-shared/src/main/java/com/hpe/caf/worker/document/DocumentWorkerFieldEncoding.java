e26c63ec8a211b3621d58cbe1adf050d2cb037bf
Dermot Hardy <dermot.hardy@hpe.com>
2017-06-19 16:40:28 +0100

    DocumentWorkerFieldEncoding: Removed unhelpful comments

--- worker-document-shared/src/main/java/com/hpe/caf/worker/document/DocumentWorkerFieldEncoding.java
+++ worker-document-shared/src/main/java/com/hpe/caf/worker/document/DocumentWorkerFieldEncoding.java
@@ -17,10 +17,7 @@ package com.hpe.caf.worker.document;
 
 public enum DocumentWorkerFieldEncoding
 {
-    // UFT8 Encoding to be used.
     utf8,
-    // Base64 Encoding to be used.
     base64,
-    // StorageRef Encoding to be used.
     storage_ref
 }
