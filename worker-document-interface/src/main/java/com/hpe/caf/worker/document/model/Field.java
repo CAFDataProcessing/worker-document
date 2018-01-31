5c988856a726bc5d396dd7fb80b4c10a45f1e28a
rado-straka <35526049+rado-straka@users.noreply.github.com>
2018-01-31 13:50:37 +0000

    SCMOD-3067: Added set methods to the Field to firstly clear old value… (#20)
    * SCMOD-3067: Added set methods to the Field to firstly clear old values, then add new one

--- worker-document-interface/src/main/java/com/hpe/caf/worker/document/model/Field.java
+++ worker-document-interface/src/main/java/com/hpe/caf/worker/document/model/Field.java
@@ -26,6 +26,7 @@ public interface Field extends DocumentWorkerObject
 {
     /**
      * Adds the specified value to this field.<p>
+     * To replace existing values by a new one use {@link #set(java.lang.String)}.
      * The value will be stored using the UTF-8 encoding.
      *
      * @param data the value to be stored
@@ -34,6 +35,7 @@ public interface Field extends DocumentWorkerObject
 
     /**
      * Adds the specified binary blob value to this field.
+     * To replace existing values by a new one use {@link #set(byte[])}.
      *
      * @param data the data to be stored
      */
@@ -41,6 +43,7 @@ public interface Field extends DocumentWorkerObject
 
     /**
      * Associates data stored in the remote data store with this field.<p>
+     * To replace existing values by a new one use {@link #setReference(java.lang.String)}.
      * The Worker Framework has the concept of a remote data store and fields can hold data in the data store as an alternative to passing
      * it around using the standard messaging system. This is recommended if the data is large.
      *
@@ -99,6 +102,32 @@ public interface Field extends DocumentWorkerObject
      */
     boolean hasValues();
 
+    /**
+     * Sets the value in this field.<p>
+     * Removes existing values by {@link #clear()} and adds new one by {@link #add(java.lang.String)}.
+     *
+     * @param data the value to be stored
+     */
+    void set(String data);
+
+    /**
+     * Sets the binary blob value in this field.<p>
+     * Removes existing values by {@link #clear()} and adds new one by {@link #add(byte[])}.
+     *
+     * @param data the data to be stored
+     */
+    void set(byte[] data);
+
+    /**
+     * Sets the reference in this field to the data stored in the remote data store.<p>
+     * Removes existing values by {@link #clear()} and adds new one by {@link #addReference(java.lang.String)}.
+     * The Worker Framework has the concept of a remote data store and fields can hold data in the data store as an alternative to passing
+     * it around using the standard messaging system. This is recommended if the data is large.
+     *
+     * @param dataRef the reference to the data in the remote data store
+     */
+    void setReference(String dataRef);
+    
     /**
      * Resets the field back to its original state, undoing any changes made to it using the add() or clear() methods.
      */
