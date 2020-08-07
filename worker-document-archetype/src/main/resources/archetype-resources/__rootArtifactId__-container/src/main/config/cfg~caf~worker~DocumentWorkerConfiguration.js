959e5186e24a08e7c1e8edf4fe8fe417145b6545
Dermot Hardy <dermot.hardy@microfocus.com>
2020-08-07 20:04:31 +0000

    SCMOD-4887: Added new reject queue facility to the archetype

--- worker-document-archetype/src/main/resources/archetype-resources/__rootArtifactId__-container/src/main/config/cfg~caf~worker~DocumentWorkerConfiguration.js
+++ worker-document-archetype/src/main/resources/archetype-resources/__rootArtifactId__-container/src/main/config/cfg~caf~worker~DocumentWorkerConfiguration.js
@@ -20,6 +20,8 @@
     outputQueue: getenv("CAF_WORKER_OUTPUT_QUEUE")
             || (getenv("CAF_WORKER_BASE_QUEUE_NAME") || getenv("CAF_WORKER_NAME") || "worker") + "-out",
     failureQueue: getenv("CAF_WORKER_FAILURE_QUEUE") || undefined,
+    rejectQueue: getenv("CAF_WORKER_REJECT_QUEUE")
+            || (getenv("CAF_WORKER_BASE_QUEUE_NAME") || getenv("CAF_WORKER_NAME") || "worker") + "-reject",
     threads: getenv("CAF_WORKER_THREADS") || 1,
     maxBatchSize: getenv("CAF_WORKER_MAX_BATCH_SIZE") || undefined,
     maxBatchTime: getenv("CAF_WORKER_MAX_BATCH_TIME") || undefined,
