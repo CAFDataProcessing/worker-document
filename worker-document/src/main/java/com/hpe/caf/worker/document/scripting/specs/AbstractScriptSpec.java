00f62a9823f0e5abb19575d6876a3e2b3217883c
Dom Gibson <dominic.gibson@microfocus.com>
2020-09-08 09:09:40 +0100

    remove random import

--- worker-document/src/main/java/com/hpe/caf/worker/document/scripting/specs/AbstractScriptSpec.java
+++ worker-document/src/main/java/com/hpe/caf/worker/document/scripting/specs/AbstractScriptSpec.java
@@ -19,7 +19,6 @@ import com.hpe.caf.worker.document.DocumentWorkerScript;
 import com.hpe.caf.worker.document.exceptions.InvalidScriptException;
 import com.hpe.caf.worker.document.impl.ApplicationImpl;
 import com.hpe.caf.worker.document.model.ScriptEngineType;
-import com.sun.tools.javac.resources.compiler;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
